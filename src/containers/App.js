ff8c1d1ae33f6151db9d2712b9b6f369a4c25ba6
wailorman <wailorman@gmail.com>
2016-01-21 17:46:36 +0300

    feat(App): Connect to store

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,38 @@
 import Message from '../components/Message'
 import TypingBox from '../components/TypingBox'
+import { sendMessage } from '../actions'
+import reducer from '../reducer'
+import store from '../store'
+
 
 const App = React.createClass({
-    handleMessageSend(args) {},
+
+    propTypes: {
+        store: React.PropTypes.object.isRequired
+    },
+
+    componentWillMount(){
+        this.props.store.subscribe(()=> {
+            this.forceUpdate();
+        });
+    },
+
+    handleMessageSend(args) {
+        let messageText = args.messageText;
+        this.props.store.dispatch(sendMessage(messageText));
+    },
 
     render() {
+        const messages = this.props.store.getState().messages;
+
         return (
             <div>
-                <Message author="Mike" text="Hey, noobs"/>
-                <TypingBox onSend={}/>
+                {messages.map((message)=> {
+                    return (
+                        <Message key={message.id} author={message.author} messageText={message.messageText}/>
+                    );
+                })}
+                <TypingBox onSend={this.handleMessageSend}/>
             </div>
         );
     }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import App from './containers/App'
-
+import { Provider } from 'react-redux'
+import store from './store'
 
 
 ReactDOM.render(
-    <App />,
+    <App store={store}/>,
     document.getElementById('app')
 );
