565bd4d8dd408347bd8f9dd8ae1fd8fc7caf5884
wailorman <wailorman@gmail.com>
2016-01-21 17:45:21 +0300

    feat(reducer): Handling SEND_MESSAGE

--- src/reducer.js
+++ src/reducer.js
@@ -1,11 +1,26 @@
 import { SEND_MESSAGE } from './action-types'
 
-export default function reducer(state = {}, action) {
+const defaultState = {
+    user: {
+        name: 'Olive'
+    },
+    messages: []
+};
+
+export default function reducer(state = defaultState, action) {
     let newState = _.cloneDeep(state);
 
     switch (action.type) {
         case SEND_MESSAGE:
-            newState.messages.push(action)
+            newState.messages.push({
+                id: _.uniqueId(),
+                messageText: action.messageText,
+                author: state.user.name
+            });
+
+            return newState;
+        default:
+            return state;
     }
 
 }
